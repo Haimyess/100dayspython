16d6cd0c287e0439db25fd0826142a4f6586bd51
haimyess <haimyess@gmail.com>
2025-02-09 16:56:26 +0200

    Hangman

--- day7.py
+++ day7.py
@@ -3,16 +3,89 @@ import random
 # Later on, we could genrate random words form a library or something. 
 word_list = ["tavi", "lisa", "millionaires", "travel", "happiness"]
 
+
 # There is a word that we randomly geenrate
 random_word =  random.choice(word_list)
+# lifes = len(random_word)
+lifes = 5
+# print(lifes -1)
+# print("Lifes", lifes)
 print(random_word)
 
-print("Guess the word!! Please choose a letter")
-user_answer = input("Type bellow your answer\n")
+# placeholder = ""
+# word_length = len(random_word)
 
-if(user_answer in random_word ):
-    print("True")
-else: print("False")
+# for position in range(word_length):
+#     placeholder += "_"
+# print(placeholder)
+
+# while lifes >= 0:
+#     lifes -= 1
+#     print("Hello")
+
+# if(user_answer in random_word ):
+#     print("True")
+# else: print("False")
+
+
+word_arr = []
+
+
+
+# while lifes > 0 :
+
+#     print("Guess the word!! Please choose a letter")
+#     user_answer = input("Type bellow your answer\n").lower()
+
+
+#     for letter in random_word:
+#         if letter == user_answer:
+#                 word_arr.append(letter)
+            
+        
+#         else:
+#                 word_arr.append("_") 
+               
+
+#     lifes -= 1
+#     print("Ramaining lifes", lifes)
+
+
+
+
+# word_arr_joined = " ".join(word_arr)
+# print(word_arr_joined)
+
+# print("".join(word_arr))
+
+# name_guess = "elefante"
+display_word = ["_"]* len(random_word)
+
+while lifes > 0 and "_" in display_word:
+    final_word = " ".join(display_word)
+    print( final_word)             
+    letter_provided = input('Guess if the letter is in the word \n')
+
+
+    for letter_index in range(len(random_word)):
+        if  letter_provided == random_word[letter_index]:
+            display_word[letter_index] =  letter_provided
+        
+
+    if  letter_provided not in random_word:
+        lifes -= 1 
+        print('Remaining lifes', lifes)
+
+
+    if "_" not in display_word: 
+        print("Congrats!!! You won!!")
+        print(final_word)
+    if lifes == 0:   print("Game Over") 
+                 
+
+
+            
+
 # We show the quantity of letters as balnks 
 # We need to guess it 
 # Each time we choose a letter, the program checks if the letter selected is included in the word  
